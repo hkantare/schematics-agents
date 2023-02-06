895dbe01c0bb14fc4f029df4e267aad9aa1036d3
athreyaks <athreyaks@in.ibm.com>
2023-02-06 12:18:18 +0530

    removed logdna

--- templates/infrastructure/observability/outputs.tf
+++ templates/infrastructure/observability/outputs.tf
@@ -12,20 +12,20 @@
 # Outputs
 ##############################################################################
 
-output "logdna_crn" {
-  description = "Logdna CRN"
-  value       = ibm_resource_instance.logdna[0].crn
-}
+# output "logdna_crn" {
+#   description = "Logdna CRN"
+#   value       = ibm_resource_instance.logdna[0].crn
+# }
 
-output "logdna_guid" {
-  description = "Logdna GUID"
-  value       = ibm_resource_instance.logdna[0].guid
-}
+# output "logdna_guid" {
+#   description = "Logdna GUID"
+#   value       = ibm_resource_instance.logdna[0].guid
+# }
 
-output "logdna_name" {
-  description = "Name of the logdna instance"
-  value       = ibm_resource_instance.logdna[0].name
-}
+# output "logdna_name" {
+#   description = "Name of the logdna instance"
+#   value       = ibm_resource_instance.logdna[0].name
+# }
 
 # output "sysdig_crn" {
 #   description = "Sysdig CRN"
